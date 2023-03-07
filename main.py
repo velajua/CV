4ea174b874742880e313909000982677e0e2c5fe
Juan Velasquez <51091959+velajua@users.noreply.github.com>
2023-03-07 10:45:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from flask import make_response
 
 def main(request):
     client = storage.Client()
-    bucket = client.get_bucket("showcv")
-    blob = bucket.blob("CV.pdf")
+    bucket = client.get_bucket("showcv")        # Name of the bucket in GCS
+    blob = bucket.blob("CV.pdf")                # Path to the file in the GCS bucket
     cv_content = blob.download_as_string()
     
     response = make_response(cv_content)
